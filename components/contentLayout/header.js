d9a1dcb992fc03f6118cde0e2eaf4a44de36fd08
Ilya Kuzmin <68997547+kuzmin-i@users.noreply.github.com>
2021-07-18 06:00:19 +0300

    add

--- components/contentLayout/header.js
+++ components/contentLayout/header.js
@@ -11,7 +11,7 @@ const Header = () => {
     return (
         <div className="header">
                 <div className="header__left">
-                    <div className="header__logo">Logo</div>
+                    <div className="header__logo"><img src="/logo.svg"/></div>
                     <div className="header__subtitle">Greenhouse Gas Emissions from Large Facilities</div>
                 </div>
                 <div className="header__right">
